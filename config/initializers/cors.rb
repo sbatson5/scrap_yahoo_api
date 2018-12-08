789b933e9f2775fb69a014f08c97f49deaa72bd5
Scott Batson <sjbats@email.com>
2018-12-08 09:56:44 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'https://infinite-forest-47028.herokuapp.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
